d9e4a2e6e64973c10d10a13b72b0bcebe70912b6
julian <julirojastoque@gmail.com>
2023-06-04 19:02:55 -0500

    carritoALl

--- src/scripts/carrito.js
+++ src/scripts/carrito.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
-
+  var user = sessionStorage.getItem("user")
   $.ajax({
-    url: "http://localhost:8080/listarCotizacion",
+    url: "http://localhost:8080/cotizacionesUsuario/"+ user,
     type: "GET",
     dataType: "JSON",
     success: function(res){
       console.log(res)
-
-      res.forEach((cotiza) => {
-
-        tablaCarrito.innerHTML += '<tr><td>'
-        + cotiza.codigo_cotizacion + '</td><td>'
-        + cotiza.codigo_prod + '</td><td>'
-        + cotiza.cantidad + '</td><td>'
-        + cotiza.total + '</td>'
-      });
+      if (res.length > 0) {
+        res.forEach((cotiza) => {
+          tablaCarrito.innerHTML += '<tr><td>'
+          + cotiza.codigo_cotizacion + '</td><td>'
+          + cotiza.Prod_Nombre + '</td><td>'
+          + cotiza.cantidad + '</td><td>'
+          + cotiza.total + '</td>'
+        });
+      } else {
+        tablaCarrito.innerHTML += "<tr><td colspan='5'><p class='text-center'>No tienes nada en carrito</p></td></tr>"
+      }
+      
     }
     
   })
